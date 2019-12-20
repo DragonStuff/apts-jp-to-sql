ecd6dfcef911d4d4f64fd818574d642cbb027586
DragonStuff <me@0xdragon.com>
2019-12-21 01:58:29 +1100

    Correctly finish when nothing available.

--- getfirst.rb
+++ getfirst.rb
@@ -4,16 +4,20 @@ DB = Sequel.connect('sqlite://apartments.db')
 
 apartments = DB[:apartments].where(rent: 0..130000, new: true)
 
-file = File.open("found_properties.txt", "w")
+if apartments.first
+    file = File.open("found_properties.txt", "w")
 
-time = Time.new
+    time = Time.new
 
-file.puts "---"
-file.puts "title: Update - " + time.day.to_s + "-" + time.month.to_s + "-" + time.year.to_s
-file.puts "assignees: DragonStuff"
-file.puts "labels: bug, enhancement"
-file.puts "---"
+    file.puts "---"
+    file.puts "title: Update - " + time.day.to_s + "-" + time.month.to_s + "-" + time.year.to_s
+    file.puts "assignees: DragonStuff"
+    file.puts "labels: bug, enhancement"
+    file.puts "---"
 
-apartments.each{|r| file.puts r[:url] }
+    apartments.each{|r| file.puts r[:url] }
 
-file.close
+    file.close
+else
+    puts "Nothing new to look at."
+end
