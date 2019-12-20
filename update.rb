b39966ed43f69dfa904f103ee4c82cfd53de99b6
DragonStuff <me@0xdragon.com>
2019-12-21 00:59:43 +1100

    Use ( instead of {.

--- update.rb
+++ update.rb
@@ -49,9 +49,9 @@ while data['page']['current_page'].to_i != data['page']['total_pages'].to_i + 1
         else
             puts "looks like I already have this one."
             if ENV["REVERSE"]
-                apartments.where{property_id == child['id'].to_i}.update(:new => true)
+                apartments.where(property_id == child['id'].to_i).update(:new => true)
             else
-                apartments.where{property_id == child['id'].to_i}.update(:new => false)
+                apartments.where(property_id == child['id'].to_i).update(:new => false)
             end
         end
     end
