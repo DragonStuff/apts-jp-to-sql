72669ca132ff474afa1d58cfce9a18271c26d0a2
DragonStuff <me@0xdragon.com>
2019-12-21 01:41:10 +1100

    Add correct format to getfirst output.

--- getfirst.rb
+++ getfirst.rb
@@ -6,6 +6,14 @@ apartments = DB[:apartments].where(rent: 0..130000, new: true)
 
 file = File.open("found_properties.txt", "w")
 
+time = Time.new
+
+file.puts "---"
+file.puts "title: Update - " + time.day.to_s + "-" + time.month.to_s + "-" + time.year.to_s
+file.puts "assignees: DragonStuff"
+file.puts "labels: bug, enhancement"
+file.puts "---"
+
 apartments.each{|r| file.puts r[:url] }
 
 file.close
