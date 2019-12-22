f9dc7baafc213b7f14a0ce2f9b9c9fce78e09902
DragonStuff <me@0xdragon.com>
2019-12-22 22:21:02 +1100

    Add vue UI component files.

---
+++ index.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app-4',
+    data: {
+        apartment: [
+            { text: 'Tokyo apartment 1' },
+            { text: 'Tokyo apartment 2' },
+            { text: 'Tokyo apartment 3' }
+        ]
+    }
+})
