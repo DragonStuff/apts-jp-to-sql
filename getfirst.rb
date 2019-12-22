4c9413c367183849281feb515af12e8f329bad35
DragonStuff <me@0xdragon.com>
2019-12-22 12:03:07 +1100

    Add native japanese requirement identifier.

--- getfirst.rb
+++ getfirst.rb
@@ -1,6 +1,13 @@
 require 'sequel'
+require 'json'
+require 'net/http'
+require 'uri'
 
 DB = Sequel.connect('sqlite://apartments.db')
+
+def openthis(url)
+    Net::HTTP.get(URI.parse(url))
+end
 
 apartments = DB[:apartments].where(rent: 0..130000, new: true)
 
@@ -12,10 +19,18 @@ if apartments.first
     file.puts "---"
     file.puts "title: Update - " + time.day.to_s + "-" + time.month.to_s + "-" + time.year.to_s
     file.puts "assignees: DragonStuff"
-    file.puts "labels: bug, enhancement"
+    file.puts "labels: enhancement"
     file.puts "---"
 
-    apartments.each{|r| file.puts r[:url] }
+    apartments.each{ |r| 
+        response = openthis(r[:url])
+
+        file.puts "---"
+        file.puts r[:url]
+        if response.include? "Conversational Japanese"
+            file.puts "JAPANESE REQUIRED."
+        end
+    }
 
     file.close
 else
